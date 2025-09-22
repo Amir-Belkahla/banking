3abebaacb915338c6e4bd8f8fecbc04099a77a4a
Amir-Belkahla <belkahla.amir01@gmail.com>
2025-09-22 13:59:14 +0100

    Implement auth, appwrite,sentry

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { sidebarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
 import path from 'path'
 import { cn } from '@/lib/utils'
+import Footer from './Footer'
 
 const Sidebar = ({user}:SiderbarProps) => {
     const pathname =  usePathname();
@@ -34,7 +35,7 @@ const Sidebar = ({user}:SiderbarProps) => {
             USER 
 
         </nav>
-        FOOTER
+        <Footer user ={user}/>
     </section>
   )
 }
---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d5091741ae57d36dff8ba6a7e023b82c@o4510063299854336.ingest.de.sentry.io/4510063302082640",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
